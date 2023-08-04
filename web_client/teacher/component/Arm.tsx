7b736c2500f6723ea4dae67de40c1dd01c53bf00
n-s-iroagba <nnamdisolomon1@gmail.com>
2023-08-04 14:09:00 +0100

    delete mobile_client directory and web_client directory,create new repository for them

--- web_client/teacher/component/Arm.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const Arm = ({ arm }: any) => {
-  return <>{arm.arm}</>
-}
-export default Arm
--- web_client/teacher/component/Form.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Form = ({form}:any)=>{
-
-    return<>{form.form}</>
-
-}
-export default Form;
